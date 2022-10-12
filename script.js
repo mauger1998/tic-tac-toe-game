a16db9386d137d5116854aef1433a9d3ba476c5c
mauger1998 <110930037+mauger1998@users.noreply.github.com>
2022-10-12 05:41:43 +0100

    Remove everything starting over

--- script.js
+++ script.js
@@ -3,7 +3,11 @@ const mainParent = document.querySelector(".appendBoardTo");
 
 /*Gameboard Object (Module)*/
 
-const gameBoard = new Array ("x", "o", "x", "o", "x", "o", "x", "o", "x" )
+
+/*Player Factories*/
+
+
+
 
     
 
@@ -12,15 +16,6 @@ const gameBoard = new Array ("x", "o", "x", "o", "x", "o", "x", "o", "x" )
 
 
 
-/* Players Objects (Factories) */
-const playerFactory = (name, token) => {
-    const player = {
-        name,
-        token
-
-    }
-    return player;
-} 
 
 
 
@@ -29,16 +24,4 @@ const playerFactory = (name, token) => {
 
 
 /*Gameboard Render Function*/
-function renderGameBoard(gameBoard) {
-    for (let i = 0; i < 9; i++) {
-        let newSpace = document.createElement("div");
-        mainParent.appendChild(newSpace);
-        newSpace.textContent = gameBoard[i];
-        mainParent.classList.add("grid");
-        console.log(gameBoard[i])
 
-    } 
-    return;
-    
-};
-renderGameBoard(gameBoard);
