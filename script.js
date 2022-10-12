5abb39e5169e7be052f6bc98f7814d31ac5edd58
mauger1998 <110930037+mauger1998@users.noreply.github.com>
2022-10-12 06:04:33 +0100

    Create module to return object with board array

--- script.js
+++ script.js
@@ -2,6 +2,24 @@ const mainParent = document.querySelector(".appendBoardTo");
 
 
 /*Gameboard Object (Module)*/
+ let gameBoardModule = (() => {
+    let gameBoard = {
+            board: [
+            "x",
+            "o",
+            "x",
+            "o",
+            "x",
+            "o",
+            "x",
+            "o",
+            "x"
+
+        ]
+           
+    } 
+    return gameBoard;
+ })();
 
 
 /*Player Factories*/
